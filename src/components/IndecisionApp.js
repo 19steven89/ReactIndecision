cfe1e948c347771cf8f3c53faab1666f52ed16e6
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-09-04 21:15:54 +0100

    added npm react modul to display a modul to the user rather than a browser alert box. The modul will contain the random option when the user clicks "what should i do?". Also set up the button handler using props passed down from the indecisionApp into OptionModul component.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,13 +3,22 @@ import AddOption from "./AddOption.js";
 import Action from "./Action.js";
 import Header from "./Header.js";
 import Options from "./Options.js";
+import OptionModal from "./OptionModal";
 
 //used as a React component called from the jsx const below. Components must begin with upper case.
 export default class IndecisionApp extends React.Component{
 
     state = {
-        options: []
+        options: [],
+        //pass the selectedOption property as a prop down into theOptionModal component below, to toggle the modal when user selects an option to choose
+        selectedOption: undefined
     };
+
+    handleClearSelectedOptionState = () => {
+        this.setState(() => ({
+            selectedOption: undefined
+        }));
+    }
 
     handleDeleteOption = (optionToRemove) => {
         //console.log("HDO", option);
@@ -21,7 +30,10 @@ export default class IndecisionApp extends React.Component{
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * (this.state.options.length));
         var option = this.state.options[randomNum];
-        alert(option);
+        // set the selected option = to a random option from the option var above, and display is the modal
+        this.setState(() => ({
+            selectedOption: option
+        }));
     }
 
     handleAddOption = (option) => {
@@ -85,6 +97,9 @@ export default class IndecisionApp extends React.Component{
                 handleDeleteOption={this.handleDeleteOption}/>
                 <AddOption
                 handleAddOption={this.handleAddOption}/>
+                <OptionModal 
+                selectedOption={this.state.selectedOption}
+                handleClearSelectedOptionState={this.handleClearSelectedOptionState}/>
              </div>
         );
     }
