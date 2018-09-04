df9f906663086aab0c8cb0fb20a5b4729fbfa29c
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-09-04 21:17:56 +0100

    OptionModul component added for displaying the random option to the user

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,6 +3,7 @@ import Modal from "react-modal";
 
 const OptionModal = (props) => (
     // set boolean val for props.selectedOption
+    // content label is used to let the user click outside of the modul to close it, rather than have to click the button
     <Modal
         isOpen = {!!props.selectedOption}
         onRequestClose = {props.handleClearSelectedOptionState}
