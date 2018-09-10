6b37d5c06a455242d1f77fd4f16cee45bc280741
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-09-10 21:12:00 +0100

    finished styling the app including all of the components and the 3rd party module which is the modal. Also added extra scss for supporting mobile devices.

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,8 +3,8 @@ import React from "react";
 const Option = (props) => (
     // add the optiontext prop defined in the parent class: Options defined above, used to display each option to the user
     // the button uses 2 css classes for styling. button and button--link   
-        <div>
-            {props.optionText}
+        <div className="option">
+            <p className="option__text">{props.count}. {props.optionText}</p>
             <button className="button button--link"
                     onClick={(e) => {
                         props.handleDeleteOption(props.optionText);
--- src/components/Options.js
+++ src/components/Options.js
@@ -4,15 +4,21 @@ import Option from "./Option.js";
 const Options = (props) => (
     //use the JS method bind to bind the "this" keyword to be in the same context as the this keyword used below
         <div>
-        <button className="button button--link" onClick={props.handleDeleteOptions}>Remove All</button>
-            {props.options.length === 0 && <p>Please add an option to get started!</p>}
+            <div className="widget-header">
+                <h3 className="widget-header__title">Your Options</h3>
+
+                <button className="button button--link" onClick={props.handleDeleteOptions}>Remove All</button>
+
+            </div>
+            {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
             {
                 //add the options from the array to the list displayed to the user using the prop defined above in the IndecisionApp class
-                props.options.map((option) => (
+                props.options.map((option, index) => (
                     //imported Option to use the Option component below
                     <Option 
                         key ={option} 
                         optionText={option}
+                        count={index+1}
                         handleDeleteOption={props.handleDeleteOption}
                     />
                 )
